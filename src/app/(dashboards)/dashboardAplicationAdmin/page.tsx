728e4f082601f4c22a21de34cc148be40b276cb1
OLuciani <lucianioscar1@gmail.com>
2025-01-27 23:16:23 -0300

    Implemento lazy loading en los dashboards para optimizar el tiempo de carga inicial

--- src/app/(dashboards)/dashboardAplicationAdmin/page.tsx
+++ src/app/(dashboards)/dashboardAplicationAdmin/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+/* "use client"
 import React, { useState, useContext, useEffect } from "react";
 import axios, { AxiosError } from "axios";
 import { DiscountsList } from "@/types/discountTypes";
@@ -76,11 +76,6 @@ const DashboardAplicationAdmin: React.FC = () => {
 
   return (
     <>
-      {/* <TokenExpiredModal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-      /> */}
-
       <div className="flex flex-col lg:flex-row lg:min-h-screen">
         <div className="lg:flex">
           <SidebarDashboardAppAdmin setSection={setSection} section={section} setReduceHeight={setReduceHeight} />
@@ -94,4 +89,108 @@ const DashboardAplicationAdmin: React.FC = () => {
   );
 };
 
+export default DashboardAplicationAdmin; */
+
+
+
+"use client";
+import React, { useState, useContext, useEffect, Suspense } from "react";
+import axios, { AxiosError } from "axios";
+import { DiscountsList } from "@/types/discountTypes";
+import { Context } from "@/context/Context";
+import Cookies from "js-cookie";
+import { isAfter, format } from "date-fns";
+import SidebarDashboardAppAdmin from "@/components/dashboardAppAdminComponents/sidebarDashboardAppAdmin/SidebarDashboardAppAdmin";
+
+// Lazy load components
+//const TokenExpiredModal = React.lazy(() => import('@/components/tokenExpiredModal/TokenExpiredModal'));
+const PendingUsers = React.lazy(() => import('@/components/dashboardAppAdminComponents/pendingUsers/PendingUsers'));
+const AllUsers = React.lazy(() => import('@/components/dashboardAppAdminComponents/allUsersComponents/AllUsers'));
+const RoleManagement = React.lazy(() => import('@/components/dashboardAppAdminComponents/roleManagement/RoleManagement'));
+const Notifications = React.lazy(() => import('@/components/dashboardAppAdminComponents/notifications/Notifications'));
+const ActivityLogs = React.lazy(() => import('@/components/dashboardAppAdminComponents/activityLogs/ActivityLogs'));
+//const SidebarDashboardAppAdmin = React.lazy(() => import('@/components/dashboardAppAdminComponents/sidebarDashboardAppAdmin/SidebarDashboardAppAdmin'));
+
+interface ErrorResponse {
+  error: string;
+}
+
+const DashboardAplicationAdmin: React.FC = () => {
+  const { userToken, setUserToken, isLoggedIn, setUserRole, setUserName, setBusinessName, setBusinessType, setSelectedOption, setUserStatus } = useContext(Context);
+  const [section, setSection] = useState<string>("pendingUsers");
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [discountsArrayList, setDiscountsArrayList] = useState<DiscountsList[]>([]);
+  const [totalDiscounts, setTotalDiscounts] = useState<number>(0);
+  const [reduceheight, setReduceHeight] = useState<boolean>(true); // Esta variable la utilizo para reducir el el espacio entre el Sidebar y renderSection en el dashboard cuando la pantalla es pequeña (menor a lg).
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      const storedUserToken = Cookies.get("userToken") || "";
+      setUserToken(storedUserToken);
+    }
+  }, [isLoggedIn, setUserToken]);
+
+  useEffect(() => {
+    const storedUserToken = Cookies.get("userToken") || "";
+    setUserToken(storedUserToken);
+
+    const cookieUserRole = Cookies.get("userRole") || "";
+    setUserRole(cookieUserRole);
+
+    const cookieUserName = Cookies.get("userName") || "";
+    setUserName(cookieUserName);
+
+    const cookieBusinessName = Cookies.get("businessName") || "";
+    setBusinessName(cookieBusinessName);
+
+    const cookieBusinessType = Cookies.get("businessType") || "";
+    setBusinessType(cookieBusinessType);
+
+    const cookieUserStatus = Cookies.get("userStatus") || "";
+    setUserStatus(cookieUserStatus);
+
+    setSelectedOption("Mi cuenta");
+  }, [setSelectedOption, setBusinessName, setBusinessType, setUserName, setUserRole, setUserToken, setUserStatus]);
+
+  const renderSection = () => {
+    switch (section) {
+      case "pendingUsers":
+        return <PendingUsers />;
+      case "allUsers":
+        return <AllUsers />;
+      case "roleManagement":
+        return <RoleManagement />;
+      case "notifications":
+        return <Notifications />;
+      case "activityLogs":
+        return <ActivityLogs />;
+      default:
+        return <PendingUsers />;
+    }
+  };
+
+  return (
+    <>
+      {/* <TokenExpiredModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      /> */}
+      <div className="flex flex-col lg:flex-row lg:min-h-screen">
+        <div className="lg:flex">
+          <Suspense fallback={<div>Loading Sidebar...</div>}>
+            <SidebarDashboardAppAdmin setSection={setSection} section={section} setReduceHeight={setReduceHeight} />
+          </Suspense>
+        </div>
+
+        <main className={`flex-grow min-h-screen p-2 lg:p-6 bg-gray-100 ${reduceheight ? "mt-[57px]" : "mt-0"} lg:mt-0`}>
+          <Suspense fallback={<div>Loading Content...</div>}>
+            {renderSection()}
+          </Suspense>
+        </main>
+      </div>
+    </>
+  );
+};
+
 export default DashboardAplicationAdmin;
+
--- src/app/(dashboards)/dashboardBusinessAdmin/page.tsx
+++ src/app/(dashboards)/dashboardBusinessAdmin/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+/* "use client"
 import React, { useState, useContext, useEffect } from "react";
 import axios, { AxiosError } from "axios";
 import { discountsList } from "@/api/discountService";
@@ -100,9 +100,6 @@ const DashboardBusinessAdmin: React.FC = () => {
         console.log("Valor de userToken en fetchDiscounts: ", userToken);
         const response = await discountsList();
 
-        /* if (response === "Token inválido o expirado en discountList") {
-          setIsModalOpen(true); 
-        } */
         if (typeof response !== "string") {
           const now = new Date();
           const validDiscounts = response.filter(
@@ -154,26 +151,183 @@ const DashboardBusinessAdmin: React.FC = () => {
   
 return (
   <>
-    {/* <TokenExpiredModal
-      isOpen={isModalOpen}
-      onClose={() => setIsModalOpen(false)}
-    /> */}
-
-    {/* <AsociatedBusinessUserDetail setSection={setSection} /> */}
-
     <div className="flex flex-col lg:flex-row lg:min-h-screen">
-      {/* <div className="absolute right-0 w-[220px] lg:block lg:relative"> */}
       <div className="lg:flex">
         <SidebarBusinessAdminDashboard setSection={setSection} section={section} setReduceHeight={setReduceHeight} />
       </div>
         
-      {/* <main className="flex-grow min-h-screen p-2 lg:p-6 bg-gray-100 mt-[57px] lg:mt-0"> */}
       <main className={`flex-grow min-h-screen p-2 lg:p-6 bg-gray-100 ${reduceheight ? "mt-[57px]" : "mt-0"} lg:mt-0`}>
         {renderSection()}
       </main>
     </div>
   </>
 );
+};
+
+export default DashboardBusinessAdmin; */
+
+
+
+"use client";
+import React, { useState, useContext, useEffect } from "react";
+import axios, { AxiosError } from "axios";
+import { discountsList } from "@/api/discountService";
+import { DiscountsList } from "@/types/discountTypes";
+import SidebarBusinessAdminDashboard from "@/components/dashboardBusinessAdminComponents/sidebarBusinessAdminDashboard/SidebarBusinessAdminDashboard";
+import { Context } from "@/context/Context";
+import Cookies from "js-cookie";
+import { isAfter, format } from "date-fns";
+import dynamic from "next/dynamic";
+
+// Importaciones dinámicas (lazy loading)
+const Overview = dynamic(() => import("@/components/dashboardBusinessAdminComponents/overview/Overview"));
+const ActiveDiscountsOverview = dynamic(() => import("@/components/dashboardBusinessAdminComponents/activeDiscountsOverview/ActiveDiscountsOverview"));
+const EffectiveSales = dynamic(() => import("@/components/dashboardBusinessAdminComponents/effectiveSales/EffectiveSales"));
+const UserNotifications = dynamic(() => import("@/components/userNotifications/UserNotifications"));
+const AsociatedBusinessUsers = dynamic(() => import("@/components/dashboardBusinessAdminComponents/allUsersAsociatesToOneBusiness/AsociatedBusinessUsers"));
+const ActiveDiscountsGallery = dynamic(() => import("@/components/dashboardBusinessAdminComponents/myActiveDiscounts/activeDiscountsGallery/ActiveDiscountsGallery"));
+const DiscountCreate = dynamic(() => import("@/components/dashboardBusinessAdminComponents/discountCreate/DiscountCreate"));
+const EditAccount = dynamic(() => import("@/components/dashboardBusinessAdminComponents/editAccount/EditAccount"));
+const InvitationBusinessEmployeeUser = dynamic(() => import("@/components/dashboardBusinessAdminComponents/invitationBusinessEmployeeUser/InvitationBusinessEmployeeUser"));
+const InvitationExtraBusinessAdminUser = dynamic(() => import("@/components/dashboardBusinessAdminComponents/invitationExtraBusinessAdminUser/InvitationExtraBusinessAdminUser"));
+// const TokenExpiredModal = dynamic(() => import("@/components/tokenExpiredModal/TokenExpiredModal"), { ssr: false });
+
+interface ErrorResponse {
+  error: string;
+}
+
+const DashboardBusinessAdmin: React.FC = () => {
+  const { userToken, setUserToken, isLoggedIn, setUserRole, setUserName, setBusinessName, setBusinessType, setUserStatus, setSelectedOption } = useContext(Context);
+  const [section, setSection] = useState<string>("resumen");
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [discountsArrayList, setDiscountsArrayList] = useState<DiscountsList[]>([]);
+  const [totalDiscounts, setTotalDiscounts] = useState<number>(0);
+  const [reduceheight, setReduceHeight] = useState<boolean>(true); // Esta variable la utilizo para reducir el el espacio entre el Sidebar y renderSection en el dashboard cuando la pantalla es pequeña (menor a lg).
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      const storedUserToken = Cookies.get("userToken") || "";
+      setUserToken(storedUserToken);
+    }
+  }, [isLoggedIn, setUserToken]);
+
+  useEffect(() => {
+    const storedUserToken = Cookies.get("userToken") || "";
+    setUserToken(storedUserToken);
+
+    const cookieUserRole = Cookies.get("userRole") || "";
+    setUserRole(cookieUserRole);
+
+    const cookieUserName = Cookies.get("userName") || "";
+    setUserName(cookieUserName);
+
+    const cookieBusinessName = Cookies.get("businessName") || "";
+    setBusinessName(cookieBusinessName);
+
+    const cookieBusinessType = Cookies.get("businessType") || "";
+    setBusinessType(cookieBusinessType);
+
+    const cookieUserStatus = Cookies.get("userStatus") || "";
+    setUserStatus(cookieUserStatus);
+
+    setSelectedOption("Mi cuenta");
+  }, [setSelectedOption, setBusinessName, setBusinessType, setUserName, setUserRole, setUserToken, setUserStatus]);
+
+  const renderSection = () => {
+    switch (section) {
+      case "notificaciones":
+        return <UserNotifications />;
+      case "overview":
+        return <Overview />;
+      case "activeDiscountsOverview":
+        return <ActiveDiscountsOverview />;
+      case "sales":
+        return <EffectiveSales />;
+      case "asociatedBusinessUsers":
+        return <AsociatedBusinessUsers />;
+      case "discountCreate":
+        return <DiscountCreate setSection={setSection} section={section} />;
+      case "activeDiscountsGallery":
+        return <ActiveDiscountsGallery />;
+      case "editAccount":
+        return <EditAccount setSection={setSection} section={section} />;
+      case "invitationBusinessEmployee":
+        return <InvitationBusinessEmployeeUser setSection={setSection} setReduceHeight={setReduceHeight} />;
+      case "invitationExtraBusinessAdmin":
+        return <InvitationExtraBusinessAdminUser setSection={setSection} />;
+      default:
+        return <Overview />;
+    }
+  };
+
+  const fetchDiscounts = async () => {
+    try {
+      if (userToken) {
+        console.log("Valor de userToken en fetchDiscounts: ", userToken);
+        const response = await discountsList();
+
+        if (typeof response !== "string") {
+          const now = new Date();
+          const validDiscounts = response.filter(
+            (discount) =>
+              !discount.validityPeriod ||
+              !isAfter(
+                now,
+                new Date(discount.startDateTime).setDate(
+                  new Date(discount.startDateTime).getDate() +
+                    discount.validityPeriod
+                )
+              )
+          );
+          setDiscountsArrayList(validDiscounts);
+        } else {
+          console.error("Error al obtener descuentos: ", response);
+        }
+      } else {
+        console.error(
+          "No se puede obtener descuentos, falta businessId o userToken"
+        );
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        const axiosError = error as AxiosError<ErrorResponse>;
+        const errorMessage =
+          axiosError.response?.data.error ||
+          "Error en la solicitud de actualización";
+        console.error("Error al obtener descuentos: ", errorMessage);
+      } else {
+        console.error("Error desconocido al obtener descuentos: ", error);
+      }
+    } 
+  };
+
+  useEffect(() => {
+    if (userToken) {
+      fetchDiscounts();
+    }
+  }, [userToken]);
+
+  useEffect(() => {
+    if (discountsArrayList.length > 0) {
+      setTotalDiscounts(discountsArrayList.length);
+    } else {
+      setTotalDiscounts(0); // En caso de que no haya descuentos, asegurarse de que totalDiscounts sea 0.
+    }
+  }, [discountsArrayList, setTotalDiscounts]);
+
+  return (
+    <>
+      <div className="flex flex-col lg:flex-row lg:min-h-screen">
+        <div className="lg:flex">
+          <SidebarBusinessAdminDashboard setSection={setSection} section={section} setReduceHeight={setReduceHeight} />
+        </div>
+
+        <main className={`flex-grow min-h-screen p-2 lg:p-6 bg-gray-100 ${reduceheight ? "mt-[57px]" : "mt-0"} lg:mt-0`}>
+          {renderSection()}
+        </main>
+      </div>
+    </>
+  );
 };
 
 export default DashboardBusinessAdmin;
